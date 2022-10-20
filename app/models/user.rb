6fd112594f28af0a1e7364faacc65d182fe497d5
MisterCxmpy <60076142+MisterCxmpy@users.noreply.github.com>
2022-10-20 14:19:57 +0100

    Add one-to-many association to backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true, uniqueness: { case-sensitive: false }, length: { minimum: 3, maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case-sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX}
