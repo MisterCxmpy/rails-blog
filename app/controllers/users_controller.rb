fc446b55c202a438d2d934c0c37be605edbd8579
MisterCxmpy <60076142+MisterCxmpy@users.noreply.github.com>
2022-10-21 11:46:09 +0100

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome #{@user.username}, account has successfully been created!"
+      redirect_to articles_path
+    else
+      render "new"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
