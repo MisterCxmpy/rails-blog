3ef2bab7c2d36930dcd7523872519d28382397d9
MisterCxmpy <60076142+MisterCxmpy@users.noreply.github.com>
2022-10-21 11:37:49 +0100

    Add new users signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase}
   has_many :articles
-  validates :username, presence: true, uniqueness: { case-sensitive: false }, length: { minimum: 3, maximum: 30 }
+  validates :username, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: { case-sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
